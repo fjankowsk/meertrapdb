09bb66a0c124f1de6de53495456ceff8d7702240
Fabian Jankowski <fjankowsk@gmail.com>
2018-10-16 13:23:48 +0100

    Implemente the other tables as well.

--- src/test_db.py
+++ src/test_db.py
@@ -16,7 +16,8 @@ import pony.orm as pn
 from pony.orm import db_session
 # local ones
 from db_helpers import setup_db
-from schema import (db, Observation, BeamConfig, TuseStatus, FbfuseStatus, PeriodCandidate)
+from schema import (db, Observation, BeamConfig, TuseStatus,
+                    FbfuseStatus, PeriodCandidate, SpsCandidate)
 
 # version info
 __version__ = "$Revision$"
@@ -30,23 +31,42 @@ def insert_data(task):
 
     while True:
         now = datetime.now()
+        buf = buffer('jfdsajlk')
         
         with db_session:
+            obs = Observation(
+                ra=0,
+                dec=0,
+                mainproj="Bla",
+                proj="bdfsa",
+                observer="Fabian",
+                utcstart=now,
+                utcend=now,
+                utcadded=now,
+                tobs=360.0,
+                finished=False,
+                nant=64,
+                cfreq=1400.123,
+                bw=800.0,
+                npol=1,
+                tsamp=0.1234
+            )
+
             for _ in range(500):
-                Observation(ra=0,
-                            dec=0,
-                            mainproj="Bla",
-                            proj="bdfsa",
-                            observer="Fabian",
-                            utcstart=now,
-                            utcend=now,
-                            utcadded=now,
-                            finished=False,
-                            nant=64,
-                            cfreq=1400.123,
-                            bw=800.0,
-                            npol=1,
-                            tsamp=0.1234)
+                SpsCandidate(
+                    utc=now,
+                    utcadded=now,
+                    ra=0.0,
+                    dec=0.0,
+                    beam='in0',
+                    snr=9.5,
+                    dm=1234.56,
+                    width=2.7,
+                    dynamicspectrum=buf,
+                    profile=buf,
+                    score=97.3,
+                    obs=obs
+                )
 
         print("Done. Time taken: {0}".format(datetime.now() - now))
         sleep(3)
@@ -64,7 +84,6 @@ def run_benchmark(nproc):
 
     while True:
         print("Bla.")
-
         sleep(5)
 
 #
@@ -83,6 +102,40 @@ def main():
 
         #pn.select((o.utcstart,o.cfreq,o.bw) for o in Observation).show()
         print(pn.count(o.id for o in Observation))
+    
+    now = datetime.now()
+    buf = buffer('jfdsajlk')
+
+    with db_session:
+        Observation(ra=0,
+                    dec=0,
+                    mainproj="Bla",
+                    proj="bdfsa",
+                    observer="Fabian",
+                    utcstart=now,
+                    utcend=now,
+                    utcadded=now,
+                    tobs=360.0,
+                    finished=False,
+                    nant=64,
+                    cfreq=1400.123,
+                    bw=800.0,
+                    npol=1,
+                    tsamp=0.1234)
+        
+        SpsCandidate(
+                    utc=now,
+                    utcadded=now,
+                    ra=0.0,
+                    dec=0.0,
+                    beam='in0',
+                    snr=9.5,
+                    dm=1234.56,
+                    width=2.7,
+                    dynamicspectrum=buf,
+                    profile=buf,
+                    score=97.3
+                )
 
     run_benchmark(nproc=64)
     
