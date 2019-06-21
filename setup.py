df4096457b6ce02b7ef3a48db37bb62aa1e465d6
Fabian Jankowski <fjankowsk@gmail.com>
2019-06-21 12:52:22 +0100

    Use the central version string here as well.

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
+import os.path
 from setuptools import setup
 
+def get_version():
+    version_file = os.path.join(os.path.dirname(__file__), 'meertrapdb', 'version.py')
+
+    with open(version_file, 'r') as f:
+        raw = f.read()
+
+    items = {}
+    exec(raw, None, items)
+
+    return items['__version__']
+
 setup(name='meertrapdb',
-      version='0.1',
+      version=get_version(),
       description='Database code for MeerTRAP.',
       url='https://bitbucket.org/jankowsk/meertrapdb',
       author='Fabian Jankowski',
