07912974e54527be5584d39b3ebc6e5ff248d9e8
Fabian Jankowski <fjankowsk@gmail.com>
2019-06-23 00:09:04 +0100

    Strip backticks. This SQL syntax is correct for MariaDB. Both queries take O(100 ms) or less to return on my ~1.5M entry test database.

--- scratch/sql_queries.sql
+++ scratch/sql_queries.sql
@@ -1,54 +1,54 @@
-#
-# detail view
-#
+/*
+detail view
+*/
 
-SELECT `spscandidate`.utc, `spscandidate`.snr, `spscandidate`.dm, `spscandidate`.width,
-`observation`.field_name, `beam`.number, beam.coherent, `scheduleblock`.sb_id_code
+SELECT spscandidate.utc, spscandidate.snr, spscandidate.dm, spscandidate.width,
+observation.field_name, beam.number, beam.coherent, scheduleblock.sb_id_code
 FROM spscandidate
 
 LEFT JOIN observation_spscandidate
 ON spscandidate.id=observation_spscandidate.spscandidate
-LEFT JOIN `observation`
-ON observation_spscandidate.`observation`=observation.id
+LEFT JOIN observation
+ON observation_spscandidate.observation=observation.id
 
-LEFT JOIN `beam_spscandidate`
+LEFT JOIN beam_spscandidate
 ON spscandidate.id=beam_spscandidate.spscandidate
-LEFT JOIN `beam`
-ON beam_spscandidate.beam=`beam`.id
+LEFT JOIN beam
+ON beam_spscandidate.beam=beam.id
 
-LEFT JOIN `observation_scheduleblock`
+LEFT JOIN observation_scheduleblock
 ON observation.id=observation_scheduleblock.observation
-LEFT JOINn `scheduleblock`
-ON `observation_scheduleblock`.`scheduleblock`=scheduleblock.id
+LEFT JOIN scheduleblock
+ON observation_scheduleblock.scheduleblock=scheduleblock.id
 
-WHERE `spscandidate`.`id`=1
+WHERE spscandidate.id=1
 ORDER BY spscandidate.snr DESC;
 
-#
-# grid and overview
-#
+/*
+grid and overview
+*/
 
-SELECT `spscandidate`.utc, `spscandidate`.snr, `spscandidate`.dm, `spscandidate`.width,
-`observation`.field_name, `beam`.number, beam.coherent, `scheduleblock`.sb_id_code
+SELECT spscandidate.utc, spscandidate.snr, spscandidate.dm, spscandidate.width,
+observation.field_name, beam.number, beam.coherent, scheduleblock.sb_id_code
 from spscandidate
 
 LEFT JOIN observation_spscandidate
 ON spscandidate.id=observation_spscandidate.spscandidate
-LEFT JOIN `observation`
-ON observation_spscandidate.`observation`=observation.id
+LEFT JOIN observation
+ON observation_spscandidate.observation=observation.id
 
-LEFT JOIN `beam_spscandidate`
+LEFT JOIN beam_spscandidate
 ON spscandidate.id=beam_spscandidate.spscandidate
-LEFT JOIN `beam`
-ON beam_spscandidate.beam=`beam`.id
+LEFT JOIN beam
+ON beam_spscandidate.beam=beam.id
 
-LEFT JOIN `observation_scheduleblock`
+LEFT JOIN observation_scheduleblock
 ON observation.id=observation_scheduleblock.observation
-LEFT JOIN `scheduleblock`
-ON `observation_scheduleblock`.`scheduleblock`=scheduleblock.id
+LEFT JOIN scheduleblock
+ON observation_scheduleblock.scheduleblock=scheduleblock.id
 
-WHERE `scheduleblock`.`id`=1
-AND `observation`.id=1
-AND `beam`.number between 123 and 160
+WHERE scheduleblock.id=1
+AND observation.id=1
+AND beam.number between 123 and 160
 ORDER BY spscandidate.snr DESC
-LIMIT 100 OFFSET 100;
+LIMIT 100 OFFSET 200;
