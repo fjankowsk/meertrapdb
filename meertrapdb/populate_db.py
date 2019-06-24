7d818bc0176e4e4fc2e06c3b1e609859341e9bfc
Fabian Jankowski <fjankowsk@gmail.com>
2019-06-24 03:43:32 +0100

    First try at populating the database from live candidates.

--- meertrapdb/populate_db.py
+++ meertrapdb/populate_db.py
@@ -12,13 +12,16 @@ import logging
 import random
 from time import sleep
 import os.path
+import shutil
 
+from astropy.time import Time
 from pony.orm import db_session
 
 from config_helpers import get_config
 from db_helpers import setup_db
 from db_logger import  DBHandler
 from general_helpers import setup_logging
+from parsing_helpers import parse_spccl_file
 import schema
 from schema import db
 from version import __version__
@@ -157,104 +160,100 @@ def run_insert_fake_data():
     log.info("Done. Time taken: {0}".format(datetime.now() - start))
 
 
-def insert_cadidates(params):
+def insert_cadidates(data, obs_utc_start):
     """
     Insert candidates into database.
     """
 
+    utc_format = "%Y-%m-%d_%H:%M:%S"
+
+    sb_utc_start = datetime.strptime("2019-06-24_15:45:23", utc_format)
+
     with db_session:
         # schedule blocks
         schedule_block = schema.ScheduleBlock(
-            sb_id=1,
-            sb_id_code="2019-06-21-005",
+            sb_id=2,
+            sb_id_code="20190623-0013",
             proposal_id="DDT-20190513-FC-01",
             proj_main="TRAPUM",
-            proj="commissioning",
-            utc_start=start,
-            observer = "Fabian",
-            description = "TRAPUM Test"
-        )
-
-        # observations
-        nobs = random.randint(1, 10)
-        for _ in range(nobs):
-            nbeam = random.randint(1, 390)
-
-            beam_config = schema.BeamConfig(
-                nbeam=nbeam,
+            proj="DWF run (day 1)",
+            utc_start=sb_utc_start,
+            observer = "Maciej Serylak",
+            description = "A MeerTRAP/ThunderKAT proposal with Deeper Wider Faster (Day 1/5)"
+            )
+        
+        beam_config = schema.BeamConfig(
+                nbeam=390,
                 tiling_mode='fill'
             )
 
-            obs = schema.Observation(
-                schedule_block=schedule_block,
-                field_name="PKS 1934-638",
-                boresight_ra="08:35:45.124",
-                boresight_dec="-45:35:15",
-                utc_start=start,
-                utc_end=start,
-                tobs=600.0,
-                finished=True,
-                nant=64,
-                receiver=1,
-                cfreq=1284.0,
-                bw=856.0,
-                nchan=4096,
-                npol=1,
-                tsamp=7.65607476635514e-05,
-                beam_config=beam_config
+        # observations
+        obs = schema.Observation(
+            schedule_block=schedule_block,
+            field_name="",
+            boresight_ra="08:35:45.124",
+            boresight_dec="-45:35:15",
+            utc_start=obs_utc_start,
+            utc_end=obs_utc_start,
+            tobs=600.0,
+            finished=True,
+            nant=64,
+            receiver=1,
+            cfreq=1284.0,
+            bw=856.0,
+            nchan=4096,
+            npol=1,
+            tsamp=7.65607476635514e-05,
+            beam_config=beam_config
+        )
+        
+        for item in data:
+            beam = schema.Beam(
+                number=item['beam'],
+                coherent=True,
+                source="Test source",
+                ra=item['ra'],
+                dec=item['dec'],
+                gl=0,
+                gb=0
             )
 
-            # beams
-            for beam_nr in range(nbeam):
-                beam = schema.Beam(
-                        number=beam_nr,
-                        coherent=True,
-                        source="Test source",
-                        ra="08:35:44.7",
-                        dec="-45:35:15.7",
-                        gl=123.12,
-                        gb=-23.1
-                )
+            node_nr = 0
+            node = schema.Node(
+                number=node_nr,
+                hostname="tpn-0-{0}".format(node_nr)
+            )
 
-                node_nr = beam_nr // 6
-                node = schema.Node(
-                        number=node_nr,
-                        hostname="tpn-0-{0}".format(node_nr)
-                )
+            pipeline_config = schema.PipelineConfig(
+                name="Test",
+                version="0.1",
+                dd_plan="Test",
+                dm_threshold=10.0,
+                snr_threshold=10.0,
+                width_threshold="500.0",
+                zerodm_zapping=True
+            )
+            
+            # candidates
+            cand_utc = Time(item['mjd'], format='mjd').iso
 
-                pipeline_config = schema.PipelineConfig(
-                        name="Test",
-                        version="0.1",
-                        dd_plan="Test",
-                        dm_threshold="5.0",
-                        snr_threshold="12.0",
-                        width_threshold="500.0",
-                        zerodm_zapping=True
-                )
-                
-                # candidates
-                ncand = random.randint(0, 100)
-                for _ in range(ncand):
-                    snr = random.uniform(5, 300)
-                    dm = random.uniform(5, 5000)
-                    width = random.uniform(1, 500)
-                    dynamic_spectrum = random.choice(dynamic_spectra)
+            dynamic_spectrum = item['plot_file']
 
-                    schema.SpsCandidate(
-                        utc=start.isoformat(' '),
-                        mjd=58000.123,
-                        observation=obs,
-                        beam=beam,
-                        snr=snr,
-                        dm=dm,
-                        dm_ex=0.7,
-                        width=width,
-                        node=node,
-                        dynamic_spectrum=dynamic_spectrum,
-                        profile="/raid/jankowsk/candidates/test/profile.png",
-                        heimdall_plot="/raid/jankowsk/candidates/test/hd.png",
-                        pipeline_config=pipeline_config
-                    )
+            schema.SpsCandidate(
+                utc=cand_utc,
+                mjd=item['mjd'],
+                observation=obs,
+                beam=beam,
+                snr=item['snr'],
+                dm=item['dm'],
+                dm_ex=0.7,
+                width=item['width'],
+                node=node,
+                dynamic_spectrum=dynamic_spectrum,
+                profile="/raid/jankowsk/candidates/test/profile.png",
+                heimdall_plot="/raid/jankowsk/candidates/test/hd.png",
+                pipeline_config=pipeline_config
+            )
 
 
 def run_insert_candidates():
@@ -275,22 +274,37 @@ def run_insert_candidates():
 
     glob_pattern = os.path.join(
         staging_dir,
-        "2*"
+        "2*_beam??_.spccl.log"
     )
 
-    utcs = glob.glob(glob_pattern)
+    spcll_files = glob.glob(glob_pattern)
+    log.info("Found {0} SPCCL files.".format(len(spcll_files)))
 
-    #sb_data = parse_schedule_block_data()
+    for filename in spcll_files:
+        log.info("Processing SPCCL file: {0}".format(filename))
 
-    for utc in utcs:
-        log.info("Processing UTC: {0}".format(utc))
+        utc_str = filename[:15]
+        utc_format = "%Y-%m-%d_%H:%M:%S"
+        utc = datetime.strptime(utc_str, utc_format)
+        log.info("UTC: {0}".format(utc))
+
         # 2) parse meta data
+        spccl_data = parse_spccl_file(filename)
 
         # 3) insert data into database
+        insert_cadidates(spccl_data, utc)
 
         # 4) copy candidate plots to webserver
+        #copy_to_webserver(spccl_data)
 
         # 5) move directory to processed
+        # processed_dir = os.path.join(
+        #     fsconf['ingest']['processed_dir'],
+        #     os.path.dirname(utc)
+        # )
+
+        # log.info("Processed dir: {0}".format(processed_dir))
+        #shutil.move(utc, processed_dir)
 
     log.info("Done. Time taken: {0}".format(datetime.now() - start))
 
