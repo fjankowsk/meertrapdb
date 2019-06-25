e73dc57b5e7b76f79c6f40195ee56a6e7cb43656
Fabian Jankowski <fjankowsk@gmail.com>
2019-06-25 16:03:14 +0100

    Remove verbose example output.

--- scripts/get_sb_info.py
+++ scripts/get_sb_info.py
@@ -24,9 +24,6 @@ logger.setLevel(logging.INFO)
 
 @tornado.gen.coroutine
 def main():
-    # Change URL to point to a valid portal node.  Subarray can be 1 to 4.
-    # Note: if on_update_callback is set to None, then we cannot use the
-    #       KATPortalClient.connect() method (i.e. no websocket access).
     portal_client = KATPortalClient('http://{}/api/client/{}'.
                                     format(args.host, args.sub_nr),
                                     on_update_callback=None, logger=logger)
@@ -34,51 +31,11 @@ def main():
     # Get the IDs of schedule blocks assigned to the subarray specified above.
     sb_ids = yield portal_client.schedule_blocks_assigned()
     print("\nSchedule block IDs on subarray {}\n{}".format(args.sub_nr, sb_ids))
-    # Example output:
-    #   Schedule block IDs on subarray 1:
-    #   [u'20161010-0001', u'20161010-0002', u'20161010-0003']
 
     # Fetch the details for one of the schedule blocks found.
     if len(sb_ids) > 0:
         sb_detail = yield portal_client.schedule_block_detail(sb_ids[0])
         print("\nDetail for SB {}:\n{}\n".format(sb_ids[0], sb_detail))
-        # Example output:
-        # Detail for SB 20161010-0001:
-        # {u'antennas_dry_run_alloc': u'm011',
-        #  u'id_code': u'20170208-0001',
-        #  u'obs_readiness': u'READY_TO_EXECUTE',
-        #  u'owner': u'CAM',
-        #  u'actual_end_time': None,
-        #  u'antennas_alloc': None,
-        #  u'instruction_set': u'run-obs-script ~/scripts/cam/basic-script.py '
-        #                      u'-t 20 -m 360 --proposal-id=CAM_AQF '
-        #                      u'--program-block-id=CAM_basic_script',
-        #  u'controlled_resources_alloc': None,
-        #  u'targets': u'[{"track_start_offset":73.0233476162,"target":"PKS 0023-26 '
-        #              u'| J0025-2602 | OB-238, radec, 0:25:49.16, -26:02:12.6, '
-        #              u'(1410.0 8400.0 -1.694 2.107 -0.4043)","track_duration":20.0}]',
-        #              u'scheduled_time': u'2017-02-08 11:53:34.000Z',
-        #              u'lead_operator_priority': None,
-        #              u'id': 156,
-        #              u'antenna_spec': u'm011',
-        #              u'state': u'SCHEDULED',
-        #              u'config_label': u'',
-        #              u'pb_id': None,
-        #              u'type': u'OBSERVATION',
-        #              u'actual_start_time': None,
-        #              u'description': u'Track for m011',
-        #              u'verification_state': u'VERIFIED',
-        #              u'sb_order': None,
-        #              u'sub_nr': 1,
-        #              u'desired_start_time': None,
-        #              u'sb_sequence': None,
-        #              u'expected_duration_seconds': 400,
-        #              u'action_time': u'2017-02-08 11:53:34.000Z',
-        #              u'controlled_resources_spec': u'',
-        #              u'controlled_resources_dry_run_alloc': u'',
-        #              u'notes': u'(Cloned from 20170123-0017) ',
-        #              u'outcome': u'UNKNOWN',
-        #              u'data_quality': None}
 
         # output JSON dump
         print("JSON dump:")
