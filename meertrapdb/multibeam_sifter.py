acb162d9ccc74f6219b6471aec7a7684af17c75d
Fabian Jankowski <fjankowsk@gmail.com>
2019-10-03 16:34:12 +0100

    Make this more modular and make it actually work.

--- meertrapdb/multibeam_sifter.py
+++ meertrapdb/multibeam_sifter.py
@@ -21,17 +21,15 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Multibeam candidate sifting code.')
 
-    parser.add_argument('file', type=str)
+    parser.add_argument('filename', type=str)
 
     parser.add_argument('-dm', type=float,
-                        nargs=1,
-                        default=[0.02],
+                        default=0.02,
                         help='Fractional DM tolerance. Default: 0.02')
 
     parser.add_argument('-mjd',
                         type=int,
-                        nargs=1,
-                        default=[7],
+                        default=7,
                         help='MJD is rounded off after this many decimals. Default: 7')
 
     return parser.parse_args()
@@ -40,6 +38,11 @@ def parse_args():
 def parse_spccl_file(filename):
     """
     Parse an SPCCL candidate file.
+
+    Parameters
+    ----------
+    filename: str
+        The name of the SPCCL file to load.
 
     Returns
     -------
@@ -55,17 +58,25 @@ def parse_spccl_file(filename):
     return data
 
 
-#
-# MAIN
-#
+def match_candidates(candidates, num_decimals, dm_thresh):
+    """
 
-def main():
-    args = parse_args()
+    Parameters
+    ----------
+    num_decimals: int
+        The number of decimals the detection MJDs to round to.
+    dm_thresh: float
+        The fractional DM tolerance to use for matching.
+    
+    Returns
+    -------
+    unique_cands: list (object)
+    num_matches: list (int)
+    """
 
-    candidates = parse_spccl_file(args.filename)
-
-    candidates['MJD'] = np.around(candidates['MJD'], decimals=args.mjd)
+    candidates['MJD'] = np.around(candidates['MJD'], decimals=num_decimals)
     candidates_sorted = np.sort(candidates, order=['MJD', 'DM', 'SNR'])
+
     unique_cands = []
     cand_iter = np.nditer(candidates_sorted, flags=['f_index', 'refs_ok'], order='C')
     match_line = candidates_sorted[0]
@@ -76,7 +87,7 @@ def main():
         # iterate first to match the first line
         if (candidates_sorted[cand_iter.index][1] == match_line[1]) and \
            ((candidates_sorted[cand_iter.index][2] - match_line[2]) / \
-             candidates_sorted[cand_iter.index][2] < args.dm):
+             candidates_sorted[cand_iter.index][2] < dm_thresh):
             match_cnt += 1
 
             if (candidates_sorted[cand_iter.index][4] > match_line[4]):
@@ -89,10 +100,25 @@ def main():
 
         cand_iter.iternext()
 
+    return unique_cands, num_matches
+
+
+#
+# MAIN
+#
+
+def main():
+    args = parse_args()
+
+    candidates = parse_spccl_file(args.filename)
+
+    unique_cands, num_matches = match_candidates(candidates, args.mjd, args.dm)
+
     with open ("unique_cands.txt", "w") as f:
         for i in range(len(unique_cands)):
             unique_cands[i] = '	'.join(str(x) for x in unique_cands[i])
-            f.write(unique_cands[i] + "	" + str(num_matches[i])+"\n")
+            info = "{0} {1}\n".format(unique_cands[i], num_matches[i])
+            f.write(info)
 
 
 if __name__ == "__main__":
