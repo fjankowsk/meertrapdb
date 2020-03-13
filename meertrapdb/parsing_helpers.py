f9c0495c340f8ce9e8a922c15ad0f88d161450bd
Fabian Jankowski <fjankowsk@gmail.com>
2020-03-13 15:07:27 +0000

    Implement parsing of version 2 SPCCL candidate files. These have the beam mode (C or I) specified. Add option to config file to specify the version.

--- meertrapdb/parsing_helpers.py
+++ meertrapdb/parsing_helpers.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 #
-#   2019 Fabian Jankowski
+#   2019 - 2020 Fabian Jankowski
 #   Parsing related helper functions.
 #
 
 import os.path
 
 import numpy as np
+from numpy.lib.recfunctions import append_fields
 
 
-def parse_spccl_file(filename):
+def parse_spccl_file(filename, version):
     """
     Parse a SPCCL file.
 
@@ -20,21 +21,67 @@ def parse_spccl_file(filename):
 
     Returns
     -------
-    data: numpy.record
+    data: ~numpy.record
         The parsed data.
+    version: int
+        The spccl version to assume.
+
+    Raises
+    ------
+    RuntimeError
+        If the file does not exist.
+    NotImplementedError
+        If the requested SPCCL version is not implemented.
     """
 
+    # check if file exists
     if not os.path.isfile(filename):
         raise RuntimeError("The SPCCL file does not exist: {0}".format(filename))
 
-    names = ['index', 'mjd', 'dm', 'width', 'snr', 'beam', 'ra', 'dec', 'fil_file', 'plot_file']
+    if version == 1:
+        names = [
+            'index', 'mjd', 'dm', 'width', 'snr', 'beam',
+            'ra', 'dec',
+            'fil_file', 'plot_file'
+        ]
+    elif version == 2:
+        names = [
+            'index', 'mjd', 'dm', 'width', 'snr', 'beam', 'beam_mode',
+            'ra', 'dec',
+            'fil_file', 'plot_file'
+        ]
+    else:
+        raise NotImplementedError('The requested SPCCL version is not implemented: {0}'.format(version))
 
-    data = np.genfromtxt(filename, dtype=None, names=names, delimiter="\t",
-                         encoding='ascii',
-                         autostrip=True)
+    temp = np.genfromtxt(
+        filename,
+        dtype=None,
+        names=names,
+        delimiter="\t",
+        encoding='ascii',
+        autostrip=True
+    )
     
     # treat case where the file is empty
-    data = np.atleast_1d(data)
+    temp = np.atleast_1d(temp)
+
+    # sanity check that the version is correct
+    if version == 2:
+        if np.unique(temp['beam_mode']) in ['C', 'I']:
+            pass
+        else:
+            raise RuntimeError('Incorrect SPCCL version detected.')
+
+    # add coherent flag
+    coherent = np.zeros(len(temp), dtype=bool)
+
+    data = append_fields(temp, 'coherent', coherent)
+
+    if version == 1:
+        data['coherent'] = True
+    elif version >= 2:
+        mask = (np.char.lower(temp['beam_mode']) == 'c')
+        data['coherent'][mask] = True
 
     # ensure a unique running index
     data['index'] = range(len(data))
@@ -43,13 +90,22 @@ def parse_spccl_file(filename):
 
 
 if __name__ == "__main__":
-    filename = os.path.join(
+    cand_v1 = os.path.join(
         os.path.dirname(__file__),
         'test',
-        'used_candidates.spccl.extra'
+        'candidates_v1.spccl.log'
     )
 
-    data = parse_spccl_file(filename)
+    cand_v2 = os.path.join(
+        os.path.dirname(__file__),
+        'test',
+        'candidates_v2.spccl.log'
+    )
 
-    for item in data:
-        print(item)
+    for filename, version in zip([cand_v1, cand_v2], [1, 2]):
+        print('Filename: {0}'.format(filename))
+        print('Version: {0}'.format(version))
+        data = parse_spccl_file(filename, version)
+
+        for item in data:
+            print(item)
