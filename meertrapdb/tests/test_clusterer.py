f85c21258bf6728a1009bc59828a02ba7428a548
Fabian Jankowski <fjankowsk@gmail.com>
2020-08-15 14:05:29 +0100

    Compare the clusterer output from a test file with a known-good sample.

--- meertrapdb/tests/test_clusterer.py
+++ meertrapdb/tests/test_clusterer.py
@@ -5,30 +5,36 @@
 
 import os.path
 
+import numpy as np
+
 from meertrapdb.parsing_helpers import parse_spccl_file
 from meertrapdb.multibeam_sifter import match_candidates
 
 
 def test_multibeam_clusterer():
-    # XXX: we need a file with duplicates
+    # known good output
+    good_filename = os.path.join(
+        os.path.dirname(__file__),
+        'test_clusterer_good_info.npy'
+    )
+    good_info = np.load(good_filename)
+
     spccl_file = os.path.join(
         os.path.dirname(__file__),
-        'candidates_v2.spccl.log'
+        'test_clusterer_candidates.spccl.log'
     )
 
-    candidates = parse_spccl_file(spccl_file, 2)
+    candidates = parse_spccl_file(spccl_file, 1)
     print('Number of candidates: {0}'.format(len(candidates)))
 
     info = match_candidates(candidates, 10.0, 0.02)
 
     mask = info['is_head']
     heads = candidates[mask]
-    num_matches = info['beams'][mask]
 
     print('Cluster heads: {0}'.format(len(heads)))
 
-    for head, nmatch in zip(heads, num_matches):
-        print('{0} {1}'.format(head, nmatch))
+    np.testing.assert_equal(good_info, info)
 
 
 if __name__ == '__main__':
